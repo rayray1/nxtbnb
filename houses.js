dc452f047c26140d8043b4f1c0c5ac7ad4851c78
Raymond <rrupiah@gmail.com>
2022-01-25 03:44:15 +0300

    added house component and images

---
+++ houses.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    picture: '/img/new_york.jpg',
+    type: 'Entire house',
+    town: 'New York',
+    title: 'Beautiful flat in New York!'
+  },
+  {
+    picture: '/img/amstd.jpg',
+    type: 'Entire house',
+    town: 'Amsterdam',
+    title: 'A flat in Amsterdam with a great view'
+  },
+]
